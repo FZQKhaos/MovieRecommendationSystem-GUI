c3dc4b8ee88145ff21d13a9d94b9a07bc22bc0c8
NewPhase14 <146068269+NewPhase14@users.noreply.github.com>
2024-02-20 15:58:58 +0100

    Refactoring - Deleted unused stuff

--- src/dk/easv/presentation/controller/MovieBoxController.java
+++ src/dk/easv/presentation/controller/MovieBoxController.java
@@ -1,21 +1,12 @@
 package dk.easv.presentation.controller;
 
-import dk.easv.entities.Movie;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class MovieBoxController {
 
@@ -26,11 +17,6 @@ public class MovieBoxController {
     private Label lblTitle;
     @FXML
     private Label lblYear;
-
-    @FXML
-    private void btnBoxAction(ActionEvent actionEvent) throws IOException {
-
-    }
 
     public void setLblTitle(String title){
         lblTitle.setText(title);
